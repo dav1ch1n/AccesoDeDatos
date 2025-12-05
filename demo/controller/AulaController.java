49028bd3f411937e52ede7a162b296e903a3cd7e
dav1ch1n <dgliceras@gmail.com>
2025-12-05 14:06:18 +0100

    EntregaFinal

--- demo/controller/AulaController.java
+++ demo/controller/AulaController.java
@@ -1,8 +1,12 @@
 package com.example.demo.controller;
 import org.springframework.web.bind.annotation.*;
+import com.example.demo.entity.Aula;
+import com.example.demo.service.AulaService;
 
 import java.util.List;
 
+@RestController
+@RequestMapping("/Aula")
 public class AulaController {
 
     private final AulaService aulaService;
@@ -12,13 +16,22 @@ public class AulaController {
     }
 
   // Devolver todas las aulas
-  
+  @GetMapping
+  public List<Aula> ListaCompleta(){
+    return aulaService.getAll();
+  }
   // Devolver un aula 
-
+  @GetMapping("/{id}")
+  public Aula getAulaId(@PathVariable Long id){
+    return aulaService.getOne(id);
+  }
   // añadir aula -- no se debe repetir
 
   // modificar aula
-
-  // borrar aula
   
+  // borrar aula
+  @DeleteMapping("/{id}")
+  public void borrar(@PathVariable Long id){
+    aulaService.delete(id);
+  }
 }
--- demo/controller/ReservaController.java
+++ demo/controller/ReservaController.java
@@ -1,9 +1,13 @@
 package com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.*;
+import com.example.demo.entity.Reserva;
+import com.example.demo.service.ReservaService;
 
 import java.util.List;
 
+@RestController
+@RequestMapping("/Reserva")
 public class ReservaController {
 
     private final ReservaService reservaService;
@@ -13,13 +17,22 @@ public class ReservaController {
     }
 
   // Devolver todas las reserva
-
+  @GetMapping
+  public List<Reserva> ListaCompleta(){
+    return reservaService.getAll();
+  }
   // Devolver un reserva 
-
+  @GetMapping("/{id}")
+  public Reserva getReservaId(@PathVariable Long id){
+    return reservaService.getOne(id);
+  }
   // añadir reserva -- no se debe repetir
 
   // modificar reserva
 
   // borrarreserva
-  
+  @DeleteMapping("/{id}")
+  public void borrar(@PathVariable Long id){
+    reservaService.delete(id);
+  }
 }
